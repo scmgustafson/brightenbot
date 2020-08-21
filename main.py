49ba4b1aa3735766bb197a04568da9fa4f5163a4
unknown <scmgustafson>
2020-08-20 20:18:56 -0700

    Resolve #10 Added text chat capability

--- main.py
+++ main.py
@@ -10,5 +10,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord successfully.')
+    text_channels = discord.utils.get(client.get_all_channels(), guild__name=GUILD, name='general')
+    await text_channels.send(content="Hello, World!")
 
 client.run(TOKEN)
