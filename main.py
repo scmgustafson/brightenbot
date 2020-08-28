1b4c1d33aa02174e3604601d6e6a08cd6718c80d
Seth Gustafson <sethrow1995@gmail.com>
2020-08-27 20:21:37 -0700

    squash

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ async def on_ready():
     text_channels = discord.utils.get(client.get_all_channels(), guild__name=GUILD, name='general')
     await text_channels.send(content="Hello, World!")
 
+
 @client.event
 async def on_message(message):
     for word in message.content.lower().split():
-        if word.replace(',','') in WHITELIST:
+        if word.replace(',', '') in WHITELIST:
             text_channels = discord.utils.get(client.get_all_channels(), guild__name=GUILD, name='general')
             await text_channels.send(content="What do you want?")
 
