810377aa169a0573356f1162da944e93a6d3634d
scmgustafson <69777608+scmgustafson@users.noreply.github.com>
2020-08-27 20:26:15 -0700

    Resolve #4 Add text reply to command
    Added ability to listen for a given word in a whitelist and respond back with canned message

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+WHITELIST = ["brightenbot"]
 
 client = discord.Client()
 
@@ -12,5 +13,13 @@ async def on_ready():
     print(f'{client.user} has connected to Discord successfully.')
     text_channels = discord.utils.get(client.get_all_channels(), guild__name=GUILD, name='general')
     await text_channels.send(content="Hello, World!")
+
+
+@client.event
+async def on_message(message):
+    for word in message.content.lower().split():
+        if word.replace(',', '') in WHITELIST:
+            text_channels = discord.utils.get(client.get_all_channels(), guild__name=GUILD, name='general')
+            await text_channels.send(content="What do you want?")
 
 client.run(TOKEN)
