c66610b3cd2632db53938fe1619987e1f88ea839
Justin Casso <justincasso1@gmail.com>
2021-03-21 13:32:04 -0700

    Add basic dicebot; add handler pattern

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import discord
+import random
+import re
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
@@ -12,14 +14,41 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord successfully.')
     text_channels = discord.utils.get(client.get_all_channels(), guild__name=GUILD, name='general')
-    await text_channels.send(content="Hello, World!")
 
 
 @client.event
 async def on_message(message):
-    for word in message.content.lower().split():
-        if word.replace(',', '') in WHITELIST:
+    # Every time a message is created, a number of handlers will receive the
+    # message, processing the message like an assembly line.
+    handlers = [
+        check_dice_roll
+    ]
+
+    # Call each handler function with the message
+    for func in handlers:
+        result = func(message)
+        if result:
             text_channels = discord.utils.get(client.get_all_channels(), guild__name=GUILD, name='general')
-            await text_channels.send(content="What do you want?")
+            await text_channels.send(content=result)
+
+# ===========================
+# OnMessage handlers go below
+# ===========================
+def check_dice_roll(message):
+    try:
+        # Sanitize input
+        sanitized_message = message.content.lower().replace(' ', '')
+        
+        # Does this match the pattern we expect for dice rolling?
+        is_roll_message = re.search(r'(\d+)d(\d+)', sanitized_message)
+        if is_roll_message:
+            # Extract the relevant portions from the string, cast to int
+            count = int(is_roll_message.group(1))
+            die = int(is_roll_message.group(2))
+
+            # Roll the specified dice however many times, and return the sum
+            return sum([random.randint(1, die) for roll in range(count)])
+    except:
+        return "Oops! I ran into a bug while trying to roll dice."
 
 client.run(TOKEN)
